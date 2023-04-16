a3a3d64d0cecf91506ad7fdccb1d27f6cee6b8b2
PolarView <backyardview090@gmail.com>
2023-04-16 13:18:34 +0300

    slider markup completed

--- client/src/components/slider/Slider.tsx
+++ client/src/components/slider/Slider.tsx
@@ -10,36 +10,46 @@ const Slider: React.FC = () => {
     <div className={styles.container}>
       <div className={styles.description}>
         <h2 className={styles.postType}>NEWS</h2>
-        <h1 className={styles.postTitle}>Title</h1>
-        <h3 className={styles.postDescription}>Here will go some descripiton and bla vlav lva</h3>
-        <div className={styles.postTags}>
-          <h5>economy</h5>
-          <h5>politics</h5>
-          <h5>elections</h5>
+        <h1 className={styles.postTitle}>
+          Title will also go here i mean above the description Title will also go here i mean above
+          the description
+        </h1>
+        <div className={styles.postQuoteContainer}>
+          <h3 className={styles.postQuote}>Here will go some descripiton and bla vlav lva</h3>
         </div>
-        <p className={styles.postDate}> 8 days ago</p>
+        <div className={styles.postTagsContainer}>
+          <h5 className={styles.postTag}>ECOMONY</h5>
+          <h5 className={styles.postTag}>POLITICS</h5>
+          <h5 className={styles.postTag}>ELECTIONS</h5>
+
+          <h5 className={styles.postTag}>USA</h5>
+          <h5 className={styles.postTag}>NATION</h5>
+          <h5 className={styles.postTag}>2024</h5>
+        </div>
       </div>
+      <div className={styles.postDate}> 8 days ago...</div>
+
       <div className={styles.sliderArrowsContainer}>
         <div className={styles.arrowWrapper}>
-          <HiOutlineArrowNarrowLeft />
+          <HiOutlineArrowNarrowLeft fontSize={40} />
         </div>
         <div className={styles.arrowWrapper}>
-          <HiOutlineArrowNarrowRight />
+          <HiOutlineArrowNarrowRight fontSize={40} />
         </div>
       </div>
       <div className={styles.pagination}>
         {[1, 2, 3, 4].map((item) => (
           <div className={styles.dot} key={item}>
-            <BsDot />
+            <BsDot fontSize={32} color="white" />
           </div>
         ))}
       </div>
-
+      <div className={styles.imageOverlay}></div>
       <Image
         src="https://c8.alamy.com/comp/2CWBHHE/usa-today-newspaper-reporting-on-the-911-attacks-2CWBHHE.jpg"
         layout="fill"
         alt="news preview"
-        priority
+        className={styles.silderImage}
       />
     </div>
   );
